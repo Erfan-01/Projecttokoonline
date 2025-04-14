88e68ccd6437139786c82d4b62ff207362ad47b1
Sidiqpermana-80 <m.sidiq.permana80@gmail.com>
2025-04-14 18:59:22 +0700

    Pertemuan4

--- resources/views/backend/v_layouts/app.blade.php
+++ resources/views/backend/v_layouts/app.blade.php
@@ -44,7 +44,8 @@
             <nav class="navbar top-navbar navbar-expand-md navbar-dark">
                 <div class="navbar-header" data-logobg="skin5">
                     <!-- This is for the sidebar toggle which is visible on mobile only -->
-                    <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+                    <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i
+                            class="ti-menu ti-close"></i></a>
                     <!-- ============================================================== -->
                     <!-- Logo -->
                     <!-- ============================================================== -->
@@ -54,21 +55,21 @@
                             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
                             <!-- Dark Logo icon -->
                             <img src="{{ asset('image/icon_univ_bsi.png') }}" alt="homepage" class="light-logo" />
-                           
+
                         </b>
                         <!--End Logo icon -->
-                         <!-- Logo text -->
+                        <!-- Logo text -->
                         <span class="logo-text">
-                             <!-- dark Logo text -->
-                             <img src="{{ asset('image/logo_text.png') }}" alt="homepage" class="light-logo" />
-                            
+                            <!-- dark Logo text -->
+                            <img src="{{ asset('image/logo_text.png') }}" alt="homepage" class="light-logo" />
+
                         </span>
                         <!-- Logo icon -->
                         <!-- <b class="logo-icon"> -->
-                            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-                            <!-- Dark Logo icon -->
-                            <!-- <img src="assets/images/logo-text.png" alt="homepage" class="light-logo" /> -->
-                            
+                        <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
+                        <!-- Dark Logo icon -->
+                        <!-- <img src="assets/images/logo-text.png" alt="homepage" class="light-logo" /> -->
+
                         <!-- </b> -->
                         <!--End Logo icon -->
                     </a>
@@ -78,7 +79,10 @@
                     <!-- ============================================================== -->
                     <!-- Toggle which is visible on mobile only -->
                     <!-- ============================================================== -->
-                    <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="ti-more"></i></a>
+                    <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)"
+                        data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i
+                            class="ti-more"></i></a>
                 </div>
                 <!-- ============================================================== -->
                 <!-- End Logo -->
@@ -88,15 +92,17 @@
                     <!-- toggle and nav items -->
                     <!-- ============================================================== -->
                     <ul class="navbar-nav float-left mr-auto">
-                        <li class="nav-item d-none d-md-block"><a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)" data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a></li>
+                        <li class="nav-item d-none d-md-block"><a
+                                class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)"
+                                data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a></li>
                         <!-- ============================================================== -->
                         <!-- create new -->
                         <!-- ============================================================== -->
-                        
+
                         <!-- ============================================================== -->
                         <!-- Search -->
                         <!-- ============================================================== -->
-                        
+
                     </ul>
                     <!-- ============================================================== -->
                     <!-- Right side toggle and nav items -->
@@ -105,14 +111,14 @@
                         <!-- ============================================================== -->
                         <!-- Comment -->
                         <!-- ============================================================== -->
-                        
+
                         <!-- ============================================================== -->
                         <!-- End Comment -->
                         <!-- ============================================================== -->
                         <!-- ============================================================== -->
                         <!-- Messages -->
                         <!-- ============================================================== -->
-                        
+
                         <!-- ============================================================== -->
                         <!-- End Messages -->
                         <!-- ============================================================== -->
@@ -121,19 +127,24 @@
                         <!-- User profile and search -->
                         <!-- ============================================================== -->
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic"
+                                href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 @if (Auth::user()->foto)
-                                <img src="{{ asset('storage/img-user/' . Auth::user()->foto) }}" 
-                                alt="user" class="rounded-circle" width="31">
+                                <img src="{{ asset('storage/img-user/' . Auth::user()->foto) }}" alt="user"
+                                    class="rounded-circle" width="31">
                                 @else
-                                <img src="{{ asset('storage/img-user/img-default.jpg') }}"
-                                alt="user" class="rounded-circle" width="31">
+                                <img src="{{ asset('storage/img-user/img-default.jpg') }}" alt="user"
+                                    class="rounded-circle" width="31">
                                 @endif
-                            <div class="dropdown-menu dropdown-menu-right user-dd animated">
-                                <a class="dropdown-item" href="{{ route('backend.user.edit', Auth::user()->id) }}"><i class="ti-user m-r-5 m-l-5"></i> Profil Saya</a>
-                                <a class="dropdown-item" href="" onclick="event.preventDefault(); document.getElementById('keluar-app').submit();"><i class="fa fa-power-off m-r-5 m-l-5"></i> Keluar</a>
-                                <div class="dropdown-divider"></div>
-                            </div>
+                                <div class="dropdown-menu dropdown-menu-right user-dd animated">
+                                    <a class="dropdown-item"
+                                        href="{{ route('backend.user.edit', Auth::user()->id) }}"><i
+                                            class="ti-user m-r-5 m-l-5"></i> Profil Saya</a>
+                                    <a class="dropdown-item" href=""
+                                        onclick="event.preventDefault(); document.getElementById('keluar-app').submit();"><i
+                                            class="fa fa-power-off m-r-5 m-l-5"></i> Keluar</a>
+                                    <div class="dropdown-divider"></div>
+                                </div>
                         </li>
                         <!-- ============================================================== -->
                         <!-- User profile and search -->
@@ -154,31 +165,45 @@
                 <!-- Sidebar navigation-->
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav" class="p-t-30">
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('backend.beranda') }}" aria-expanded="false">
-                            <i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Beranda</span></a>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{ route('backend.beranda') }}" aria-expanded="false">
+                                <i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Beranda</span></a>
                         </li>
-                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('backend.user.index') }}" aria-expanded="false">
-                            <i class="mdi mdi-account"></i><span class="hide-menu">User</span></a>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{ route('backend.user.index') }}" aria-expanded="false">
+                                <i class="mdi mdi-account"></i><span class="hide-menu">User</span></a>
                         </li>
-                        <li class="sidebar-item"> <a class="sidebar-link has-arrow waveseffect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                            <i class="mdi mdi-shopping"></i><span class="hide-menu">Data Produk </span></a>
+                        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{ route('backend.customer.index') }}" aria-expanded="false"><i class="mdi mdi-account-multiple"></i><span class="hide-menu">Customer</span></a>
+                        </li>
+                        <li class="sidebar-item"> <a class="sidebar-link has-arrow waveseffect waves-dark"
+                                href="javascript:void(0)" aria-expanded="false">
+                                <i class="mdi mdi-shopping"></i><span class="hide-menu">Data Produk </span></a>
                             <ul aria-expanded="false" class="collapse first-level">
-                                <li class="sidebar-item"><a href="{{ route('backend.kategori.index') }}" class="sidebar-link">
-                                    <i class="mdi mdi-chevron-right"></i><span class="hide-menu"> Kategori</span></a>
+                                <li class="sidebar-item"><a href="{{ route('backend.kategori.index') }}"
+                                        class="sidebar-link">
+                                        <i class="mdi mdi-chevron-right"></i><span class="hide-menu">
+                                            Kategori</span></a>
                                 </li>
-                                <li class="sidebar-item"><a href="{{ route('backend.produk.index') }}" class="sidebar-link">
-                                    <i class="mdi mdi-chevron-right"></i><span class="hide-menu"> Produk</span></a>
+                                <li class="sidebar-item"><a href="{{ route('backend.produk.index') }}"
+                                        class="sidebar-link">
+                                        <i class="mdi mdi-chevron-right"></i><span class="hide-menu">
+                                            Produk</span></a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                            <i class="mdi mdi-receipt"></i><span class="hide-menu">Laporan </span></a>
+                        <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark"
+                                href="javascript:void(0)" aria-expanded="false">
+                                <i class="mdi mdi-receipt"></i><span class="hide-menu">Laporan </span></a>
                             <ul aria-expanded="false" class="collapse first-level">
-                                <li class="sidebar-item"><a href="{{ route('backend.laporan.formuser') }}" class="sidebar-link">
-                                    <i class="mdi mdi-chevron-right"></i><span class="hide-menu"> User </span></a>
+                                <li class="sidebar-item"><a href="{{ route('backend.laporan.formuser') }}"
+                                        class="sidebar-link">
+                                        <i class="mdi mdi-chevron-right"></i><span class="hide-menu"> User </span></a>
                                 </li>
-                                <li class="sidebar-item"><a href="{{ route('backend.laporan.formproduk') }}" class="sidebar-link">
-                                    <i class="mdi mdi-chevron-right"></i><span class="hide-menu"> Produk </span></a>
+                                <li class="sidebar-item"><a href="{{ route('backend.laporan.formproduk') }}"
+                                        class="sidebar-link">
+                                        <i class="mdi mdi-chevron-right"></i><span class="hide-menu"> Produk
+                                        </span></a>
                                 </li>
                             </ul>
                         </li>
@@ -208,11 +233,11 @@
                 <!-- ============================================================== -->
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
-                
+
                 <!-- @yieldAwal -->
                 @yield('content')
                 <!-- @yieldAkhir-->
-                
+
                 <!-- ============================================================== -->
                 <!-- End PAge Content -->
                 <!-- ============================================================== -->
@@ -272,7 +297,7 @@
     </script>
     <!-- form keluar app -->
     <form id="keluar-app" action="{{ route('backend.logout') }}" method="POST" class="dnone">
-    @csrf
+        @csrf
     </form>
     <!-- form keluar app end -->
 
